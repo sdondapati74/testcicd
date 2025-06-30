7e4eb646a4f3a7a35bb42fba3c360d74e936b0c8
sdondapati74 <97804995+sdondapati74@users.noreply.github.com>
2025-06-30 22:17:56 +0530

    Update test.py

--- source/test.py
+++ source/test.py
@@ -1,20 +1,7 @@
 import sys
-sys.path.append('/home/runner/work/testcicd/testcicd/vector_store/VectorStore.py')
 
-# Requisite imports
-import vector_store.VectorStore  # Importing the VectorStore class from vector_store module
-import numpy as np  
+from testpackage import areafunctions.py
 
-# Establish a VectorStore instance
-vector_store = VectorStore()  # Creating an instance of the VectorStore class
+testval=rectangle(10,5)
 
-# Define sentences
-sentences = [  # Defining a list of example sentences
-    "I eat mango",
-    "mango is my favorite fruit",
-    "mango, apple, oranges are fruits",
-    "fruits are good for health",
-]
-
-# Tokenization and Vocabulary Creation
-vocabulary = set()  # Initializing an empty set to store unique words
+print(testval)
